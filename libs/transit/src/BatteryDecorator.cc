8a82978ba874ec9081963a0144cf4dfe8ab9fd8b
Madhan Mohan <mohan130@umn.edu>
2022-12-06 11:09:58 -0600

    BatteryDecorator.cc and BatteryDecorator.h base code created

--- libs/transit/src/BatteryDecorator.cc
+++ libs/transit/src/BatteryDecorator.cc
@@ -1,25 +1,22 @@
 #ifndef BATTERY_DECORATOR_H_
 #define BATTERY_DECORATOR_H_
+#include "BatteryDecorator.h"
 
 #include <vector>
 
-#include "IStrategy.h"
+#include "IEntity.h"
 
 class BatteryDecorator : public IEntity {
  public:
   BatteryDecorator(IEntity *entity) {
-    strategy = strategy_;
-    time = 0;
+    BatteryLevel = 100.0;
   }
-  // all entity functions
-  // void Update()
-  // calculate battery required to calculate distance
-  // 
+
+void Update(IEntity *entity, double dt) {}
+void CalculateBatteryLevel() {}
 
  protected:
-  IStrategy *strategy;
-  float time;
-  // BL float
-};  // close class
+  float BatteryLevel;
+};
 
 #endif  // Battery_DECORATOR_H_
