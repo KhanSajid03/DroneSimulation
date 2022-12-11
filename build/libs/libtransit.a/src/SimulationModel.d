e32ef99176ebb1b9466e65890f38327db04c3187
Madhan Mohan <mohan130@umn.edu>
2022-12-11 15:00:06 -0600

    Added battery to assets and umn.json

--- build/libs/libtransit.a/src/SimulationModel.d
+++ build/libs/libtransit.a/src/SimulationModel.d
@@ -88,7 +88,9 @@
  ../../dependencies/include/picojson.h \
  ../../dependencies/include/picojson.h include/CompositeFactory.h \
  include/IEntityFactory.h include/Drone.h include/IStrategy.h \
- include/DroneFactory.h include/RobotFactory.h include/Robot.h
+ include/DroneFactory.h include/RobotFactory.h include/Robot.h \
+ include/CreeperFactory.h include/Creeper.h include/HelicopterFactory.h \
+ include/Helicopter.h include/CarFactory.h include/Car.h
 
 include/SimulationModel.h:
 
@@ -287,3 +289,15 @@ include/DroneFactory.h:
 include/RobotFactory.h:
 
 include/Robot.h:
+
+include/CreeperFactory.h:
+
+include/Creeper.h:
+
+include/HelicopterFactory.h:
+
+include/Helicopter.h:
+
+include/CarFactory.h:
+
+include/Car.h:
