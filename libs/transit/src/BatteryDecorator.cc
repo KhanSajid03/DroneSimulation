4c5be90e92473ab353fef40585e32e395aa70976
Madhan Mohan <mohan130@umn.edu>
2022-12-12 15:18:24 -0600

    updated BatteryDecorator.cc

--- libs/transit/src/BatteryDecorator.cc
+++ libs/transit/src/BatteryDecorator.cc
@@ -2,6 +2,11 @@
 #include "BatteryDecorator.h"
 
 void Update(float dt) {
+
+// first case: if distance from drone, robot, to destination - BatteryLevel >= 0 then do trip.
+// then move and make BatteryLevel -= dt
+
+
   // float minDis = std::numeric_limits<float>::max();
   // if (entity->GetAvailability()) {
   //   // float disToEntity = this->GetPosition().Distance(entity->GetPosition()); // calculating the distance from current entity to robot
